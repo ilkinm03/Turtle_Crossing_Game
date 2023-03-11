bd896aa0c0219dcb73f0b3c3e61fc3458b8460cb
ilkinm03 <ilkinm2003@gmail.com>
2023-03-11 17:41:24 +0400

    Add functionality for moving turtle forward/backward

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import time
 from turtle import Screen
+from player import Player
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+
 screen.listen()
+screen.onkey(player.move_forwards, "Up")
+screen.onkey(player.move_backwards, "Down")
 
 game_is_on = True
 while game_is_on:
--- player.py
+++ player.py
@@ -1,7 +1,23 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -200)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player():
-    raise NotImplemented
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.left(90)
+        self.goto(STARTING_POSITION)
+
+    def move_forwards(self):
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
+
+    def move_backwards(self):
+        new_y = self.ycor() - MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
